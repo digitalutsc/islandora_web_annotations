687ca9bc1af15372e5a6e44c7a01c016dbf4c1a0
Natkeeran <natkeeran@gmail.com>
2016-11-07 12:32:52 -0500

    large image - openseadragon id

--- js/large_image/large_image.js
+++ js/large_image/large_image.js
@@ -1,8 +1,15 @@
 
 jQuery(document).ready(function() {
 
+    // Replace id
+    jQuery("#islandora-openseadragon").attr("id", "openseadragon-islandora");
+
+    Drupal.settings.islandora_open_seadragon_viewer.id = "openseadragon-islandora";
+
+
     // Get the div DOM
     var openSeaDragonDiv = jQuery("#openseadragon-islandora");
+
 
     // Remove the class
     openSeaDragonDiv.removeClass( "islandora-openseadragon" );
