aa74e0aac1b40afdbeeb36d1b3bfd83bf10aca17
Natkeeran <natkeeran@gmail.com>
2016-11-07 10:57:55 -0500

    UI prototype for large image annotations - OpenSeaDragon

---
+++ js/large_image/large_image.js
@@ -0,0 +1,20 @@
+
+jQuery(document).ready(function() {
+
+    // Get the div DOM
+    var openSeaDragonDiv = jQuery("#openseadragon-islandora");
+
+    // Remove the class
+    openSeaDragonDiv.removeClass( "islandora-openseadragon" );
+
+    // Add a parent div with this class
+    openSeaDragonDiv.wrap('<div class="islandora-openseadragon" id="openseadragon-wrapper"></div>');
+
+
+    var addButton = jQuery('<button id="add-annotation-button" onclick="anno.activateSelector();">Add Annotation</button>');
+    addButton.appendTo(jQuery("#openseadragon-wrapper"));
+
+    var os_viewer = Drupal.settings.islandora_open_seadragon_viewer;
+    anno.makeAnnotatable(os_viewer);
+
+});
