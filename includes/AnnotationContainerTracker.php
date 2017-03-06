c2032ba36ae1c8d2337dd81e1a27085171692aab
Natkeeran <natkeeran@gmail.com>
2017-03-06 16:31:47 -0500

    add cleanup to if container found case

--- includes/AnnotationContainerTracker.php
+++ includes/AnnotationContainerTracker.php
@@ -8,7 +8,6 @@
 
 class AnnotationContainerTracker {
   const MODULE_NAME = "Islandora Web Annotations";
-  private $containerList = "";
 
   function __construct() {
   }
@@ -17,19 +16,24 @@ class AnnotationContainerTracker {
     $this->getLock();
 
     $annotationContainerID = $annotationContainer->getAnnotationContainerPID($targetObjectID);
-    $this->containerList  = variable_get('islandora_web_annotations_inprocess');
-    $pos = strpos($this->containerList, $targetObjectID);
 
     // Case 1: If AnnotationContainer found, return that container id.
     if($annotationContainerID != "None") {
-    // Case 2: If AnnotationContainer ID not found, and not in the InProcess List, then create it and return it.
-    } else if ($pos === false) {
-      $list = $this->containerList . "||" .  $targetObjectID;
-      variable_set('islandora_web_annotations_inprocess', $list);
-      $annotationContainerID = $annotationContainer->createAnnotationContainer($targetObjectID, $annotationData);
-    // Case 3 : If AnnotationContainer ID not found, and is in the InProcess List, then wait until it gets index and then return it.
+      $this->cleanupInProcessList($targetObjectID);
     } else {
-      $annotationContainerID = $this->pollSolr($annotationContainer, $targetObjectID);
+      $containerList  = variable_get('islandora_web_annotations_inprocess');
+      $pos = strpos($containerList, $targetObjectID);
+
+      // Case 2: If AnnotationContainer ID not found, and not in the InProcess List, then create it and return it.
+      if ($pos === false) {
+        $list = $containerList . "||" .  $targetObjectID;
+        variable_set('islandora_web_annotations_inprocess', $list);
+        $annotationContainerID = $annotationContainer->createAnnotationContainer($targetObjectID, $annotationData);
+
+      // Case 3 : If AnnotationContainer ID not found, and is in the InProcess List, then wait until it gets index and then return it.
+      } else {
+        $annotationContainerID = $this->pollSolr($annotationContainer, $targetObjectID);
+      }
     }
 
     lock_release('getAnnotationContainer');
@@ -47,7 +51,7 @@ class AnnotationContainerTracker {
 
       if($time_elapsed_secs > 20){
         $msg = "Timeout Exception.  Taking too long to get lock for getAnnotationContainer.";
-        watchdog(AnnotationConstants::MODULE_NAME, 'Error in AnnotationTracker -> getLock: %t', array('%t' => $msg), WATCHDOG_ERROR);
+        watchdog(AnnotationContainerTracker::MODULE_NAME, 'Error in AnnotationTracker -> getLock: %t', array('%t' => $msg), WATCHDOG_ERROR);
         throw new Exception($msg);
       }
     }
@@ -69,7 +73,7 @@ class AnnotationContainerTracker {
 
         if($time_elapsed_secs > 20){
           $msg = "Timeout Exception.  Taking too long to find solr index of the AnnotationContainer for target object with PID " . $targetObjectID;
-          watchdog(AnnotationConstants::MODULE_NAME, 'Error in AnnotationTracker -> pollSolr: %t', array('%t' => $msg), WATCHDOG_ERROR);
+          watchdog(AnnotationContainerTracker::MODULE_NAME, 'Error in AnnotationTracker -> pollSolr: %t', array('%t' => $msg), WATCHDOG_ERROR);
           // Lets clean up the list, in case it got into the list
           $this->cleanupInProcessList($targetObjectID);
           throw new Exception($msg);
